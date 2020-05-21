d68fcbaca71692e506c40a67a5b8a6ed26589b07
AasthaGithub <aasthamehta2704@gmail.com>
2020-05-21 21:46:57 +0530

    init

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'A12345'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+'''
+Commands to run
+    .\Scripts\activate
+    python app.py
+    set APP_SETTINGS="config.DevelopmentConfig"
+    set DATABASE_URL="postgresql://localhost/books_store"
+    >echo  %DATABASE_URL%
+    pip install flask_sqlalchemy
+    pip install flask_script
+    pip install flask_migrate
+    pip install psycopg2-binary
+    python manage.py db init
+       
+        '''
